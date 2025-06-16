3e9ceacb4b4bf91478b943c575e7d2b881917743
MariamHLoyac <mariam@loyac.org>
2025-06-16 13:23:25 +0300

    language and time settings

--- resource_management_backend/settings.py
+++ resource_management_backend/settings.py
@@ -105,9 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+# Available languages
+LANGUAGES = (
+    ("ar", "Arabic"),
+    ("en", "English"),
+)
+
+TIME_ZONE = 'Asia/Kuwait'
 
 USE_I18N = True
 
