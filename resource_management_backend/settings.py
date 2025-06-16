214c7a57129182a0b1ca9ce470d02c9b226477c7
MariamHLoyac <mariam@loyac.org>
2025-06-16 13:11:41 +0300

    install and setup graphene-django

--- resource_management_backend/settings.py
+++ resource_management_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # ------
     'rest_framework',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Graphene setup
+GRAPHENE = {
+    "SCHEMA": "resource_management_backend.schema.schema",
+}
