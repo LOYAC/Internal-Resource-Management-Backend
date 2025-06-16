6acdbd77ef51c49a7da3bd37c5b004d6f98108ab
MariamHLoyac <mariam@loyac.org>
2025-06-16 12:57:52 +0300

    fix spelling mistake in project name

--- resource_management_backend/urls.py
+++ resource_management_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for resource_managment_backend project.
+URL configuration for resource_management_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
